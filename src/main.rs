c03e3b8e522bb01bad1ea02af26ed763f2c778d6
Jakub A. Gramsz <jakub.gramsz@nokia.com>
2019-12-30 16:00:55 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use std::error::Error;
+use std::fs;
+use std::fs::File;
+
+extern crate structopt;
+extern crate handlebars;
+extern crate serde_yaml;
+
+use structopt::StructOpt;
+use std::path::PathBuf;
+use serde_yaml::Value;
+use handlebars::Handlebars;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "hbs-cli",
+            about = "Simple handlebars CLI",
+            rename_all = "kebab-case")]
+/// Simple CLI for generating text from handlebars (or Mustache) templates,
+/// feed with data from file (YAML parser used is for it).
+struct Opt {
+    #[structopt(parse(from_os_str), name="PROPS_FILE")]
+    /// Properties file can be ether YAML or JSON (as JSON is YAMLs subset)
+    propsfile: PathBuf,
+
+    #[structopt(parse(from_os_str), name="TEMPLATE_FILE")]
+    /// Template Handlebars or Mustache (as Mustashe is Handlebars' subset)
+    template: PathBuf,
+
+    #[structopt(short,long)]
+    /// Not implemented parameter for output file name if not provided output
+    /// redirected to stdout.
+    output: Option<String>,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let opt = Opt::from_args();
+    eprintln!("{:?}", opt);
+    let reg = Handlebars::new();
+    let propsfile = File::open(opt.propsfile)?;
+    let data: Value = serde_yaml::from_reader(propsfile)?;
+    let template = fs::read_to_string(opt.template)?;
+
+    println!(
+        "{}",
+        reg.render_template(&template, &data)?
+    );
+
+    Ok(())
+}
