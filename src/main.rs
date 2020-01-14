6a1b3c5664db75d0343a71cb4f2a45fdf44585e7
Jakub A. Gramsz <jakub.gramsz@gmail.com>
2020-01-14 23:43:25 +0100

    Handle template registration
    * Add glob to dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
 use std::fs::File;
+use std::path::Path;
 
 extern crate structopt;
 extern crate handlebars;
@@ -10,6 +11,7 @@ use structopt::StructOpt;
 use std::path::PathBuf;
 use serde_yaml::Value;
 use handlebars::Handlebars;
+use glob::glob;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "hbs-cli",
@@ -30,12 +32,24 @@ struct Opt {
     /// Not implemented parameter for output file name if not provided output
     /// redirected to stdout.
     output: Option<String>,
+
+    #[structopt(short,long)]
+    /// Register all templates matching to the glob provided by the option.
+    /// Those templates can be used as partials.
+    register_glob: Option<String>,
+
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
     eprintln!("{:?}", opt);
-    let reg = Handlebars::new();
+    let mut reg = Handlebars::new();
+
+    match opt.register_glob {
+        Some(pattern) => register_templates_from_pattern(&mut reg, pattern).unwrap(),
+        None => eprintln!("No glob provided for templates registration."),
+    };
+
     let propsfile = File::open(opt.propsfile)?;
     let data: Value = serde_yaml::from_reader(propsfile)?;
     let template = fs::read_to_string(opt.template)?;
@@ -46,5 +60,29 @@ fn main() -> Result<(), Box<dyn Error>> {
         None => println!("{}", text),
     };
 
+    Ok(())
+}
+
+fn register_templates_from_pattern(
+    hbs: &mut Handlebars,
+    pattern: String
+) -> Result<(), Box<dyn Error>> {
+    eprintln!( "Registring templates matching to {:?}", pattern );
+    for entry in glob( &pattern ).expect( "Failed to read glob pattern" ) {
+        match entry {
+            Ok( path ) => {
+                let the_file = Path::new( &path );
+                if the_file.is_file() {
+                  hbs.register_template_file(
+                      the_file.file_stem()
+                              .and_then(|x| x.to_str())
+                              .unwrap(),
+                      &path
+                  )?;
+                }
+            }
+            Err(e) => eprintln!("{:?}", e)
+        }
+    }
     Ok(())
 }
