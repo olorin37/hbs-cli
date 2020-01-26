bc79905cdbe9d734f3e46be36ef8c23d3db4ea1d
Jakub A. Gramsz <jakub.gramsz@gmail.com>
2020-01-26 14:58:53 +0100

    Align e2e tests to template registration

--- fixtures/test.sh
+++ fixtures/test.sh
@@ -2,5 +2,10 @@
 
 ## run it from /fixtures
 
-../target/debug/hbs-cli data.yaml template.hbs -o a.txt
+# no templates registration
+../target/debug/hbs-cli data.yaml template.hbs -o no_registration.txt
+diff out_no_registration.txt no_registration.txt
+
+# with registration
+../target/debug/hbs-cli data.yaml template.hbs -r '**/*.hbs' -o a.txt
 diff out.txt a.txt
