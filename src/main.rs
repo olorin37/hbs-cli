fee78b0854e61b994611a684c2a8e138ed051726
Jakub A. Gramsz <jakub.gramsz@nokia.com>
2023-03-09 23:30:38 +0100

    Format code

--- src/main.rs
+++ src/main.rs
@@ -3,61 +3,66 @@ use std::fs;
 use std::fs::File;
 use std::path::Path;
 
-extern crate structopt;
 extern crate handlebars;
 extern crate serde_yaml;
+extern crate structopt;
 
-use structopt::StructOpt;
-use std::path::PathBuf;
-use serde_yaml::Value;
-use handlebars::Handlebars;
 use glob::glob;
+use handlebars::Handlebars;
+use serde_yaml::Value;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "hbs-cli",
-            about = "Simple handlebars CLI",
-            rename_all = "kebab-case")]
+#[structopt(
+    name = "hbs-cli",
+    about = "Simple handlebars CLI",
+    rename_all = "kebab-case"
+)]
 /// Simple CLI for generating text from handlebars templates, feed with data
 /// from file (YAML parser used is for it).
 struct Opt {
-    #[structopt(parse(from_os_str), name="PROPS_FILE")]
+    #[structopt(parse(from_os_str), name = "PROPS_FILE")]
     /// Properties file can be ether YAML or JSON (as JSON is YAMLs subset)
     propsfile: PathBuf,
 
-    #[structopt(parse(from_os_str), name="TEMPLATE_FILE")]
+    #[structopt(parse(from_os_str), name = "TEMPLATE_FILE")]
     /// Template Handlebars
     template: PathBuf,
 
-    #[structopt(short,long)]
+    #[structopt(short, long)]
     /// Not implemented parameter for output file name if not provided output
     /// redirected to stdout.
     output: Option<String>,
 
-    #[structopt(short,long)]
+    #[structopt(short, long)]
     /// Register all templates matching to the glob provided by the option.
     /// Those templates can be used as partials.
     register_glob: Option<String>,
 
-    #[structopt(short,long)]
+    #[structopt(short, long)]
     /// Make error output verobse
     verbose: bool,
-
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
-    if opt.verbose { eprintln!("{:?}", opt); }
+    if opt.verbose {
+        eprintln!("{:?}", opt);
+    }
     let mut reg = Handlebars::new();
 
     match opt.register_glob {
         Some(pattern) => {
             if opt.verbose {
-                eprintln!( "Registring templates matching to {:?}", pattern );
+                eprintln!("Registring templates matching to {:?}", pattern);
             }
             register_templates_from_pattern(&mut reg, pattern)?;
         }
-        None => if opt.verbose {
-            eprintln!("No glob provided for templates registration.");
+        None => {
+            if opt.verbose {
+                eprintln!("No glob provided for templates registration.");
+            }
         }
     };
 
@@ -76,22 +81,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn register_templates_from_pattern(
     hbs: &mut Handlebars,
-    pattern: String
+    pattern: String,
 ) -> Result<(), Box<dyn Error>> {
-    for entry in glob( &pattern ).expect( "Failed to read glob pattern" ) {
+    for entry in glob(&pattern).expect("Failed to read glob pattern") {
         match entry {
-            Ok( path ) => {
-                let the_file = Path::new( &path );
+            Ok(path) => {
+                let the_file = Path::new(&path);
                 if the_file.is_file() {
-                  hbs.register_template_file(
-                      the_file.file_stem()
-                              .and_then(|x| x.to_str())
-                              .unwrap(),
-                      &path
-                  )?;
+                    hbs.register_template_file(
+                        the_file.file_stem().and_then(|x| x.to_str()).unwrap(),
+                        &path,
+                    )?;
                 }
             }
-            Err(e) => eprintln!("{:?}", e)
+            Err(e) => eprintln!("{:?}", e),
         }
     }
     Ok(())
